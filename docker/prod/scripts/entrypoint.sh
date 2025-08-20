be1c5b2a08d5bf3ec0bb9a7f0b3c1a87c20d3b87
Nikita <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-08-20 21:55:59 +0300

    fix

--- docker/prod/scripts/entrypoint.sh
+++ docker/prod/scripts/entrypoint.sh
@@ -3,6 +3,6 @@ set -e
 
 chown -R appuser:appuser /app/logs
 
-gosu appuser make apply_migrations POSTGRES_DSN=postgresql://postgres:postgres@db:5432/postgres?sslmode=disable
+make apply_migrations POSTGRES_DSN=postgresql://postgres:postgres@db:5432/postgres?sslmode=disable
 
-exec gosu appuser ./app
+exec ./app
