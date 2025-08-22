a4e7e9633119afbe58ee4fcf705eda0e8f3da010
Nikita <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-08-22 21:29:12 +0300

    Remove useless models

--- migrations/20250528182140_create_tables.sql
+++ migrations/20250528182140_create_tables.sql
@@ -21,20 +21,6 @@ CREATE TABLE recurring_transactions
     created_at          TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at          TIMESTAMPTZ NOT NULL DEFAULT now()
 );
-
-CREATE TABLE period_financial_summaries
-(
-    id                   SERIAL PRIMARY KEY,
-    date_from            DATE           NOT NULL,
-    date_to              DATE           NOT NULL,
-    starting_balance     NUMERIC(12, 2) NOT NULL,
-    projected_balance    NUMERIC(12, 2),
-    actual_balance       NUMERIC(12, 2),
-    projected_net_change NUMERIC(12, 2),
-    actual_net_change    NUMERIC(12, 2),
-    created_at           TIMESTAMPTZ    NOT NULL DEFAULT now(),
-    updated_at           TIMESTAMPTZ    NOT NULL DEFAULT now()
-);
 -- +goose StatementEnd
 
 -- +goose Down
