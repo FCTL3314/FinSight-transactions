eb928f2919c40d221681605e1a1bd85476f3d3c2
Nikita <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-08-22 21:26:50 +0300

    Added ResponseFinanceDetailing schema

---
+++ pkg/schemas/detailing.go
@@ -0,0 +1,42 @@
+package schemas
+
+import "time"
+
+// TODO: Move here all schema-like struct from models
+
+type ResponseFinanceDetailing struct {
+	DateFrom         time.Time `json:"dateFrom"`
+	DateTo           time.Time `json:"dateTo"`
+	InitialAmount    float64   `json:"initialAmount"`
+	TotalIncome      float64   `json:"totalIncome"`
+	TotalExpense     float64   `json:"totalExpense"`
+	ProfitEstimated  float64   `json:"profitEstimated"`
+	ProfitReal       float64   `json:"profitReal"`
+	AfterAmountNet   float64   `json:"afterAmountNet"`
+	AfterAmountGross float64   `json:"afterAmountGross"`
+}
+
+func NewResponseFinanceDetailing(
+	dateFrom time.Time,
+	dateTo time.Time,
+	initialAmount float64,
+	totalIncome float64,
+	totalExpense float64,
+	profitReal float64,
+) *ResponseFinanceDetailing {
+	profitEstimated := totalIncome - totalExpense
+	afterAmountGross := initialAmount + totalIncome
+	afterAmountNet := afterAmountGross - totalExpense
+
+	return &ResponseFinanceDetailing{
+		DateFrom:         dateFrom,
+		DateTo:           dateTo,
+		InitialAmount:    initialAmount,
+		TotalIncome:      totalIncome,
+		TotalExpense:     totalExpense,
+		ProfitEstimated:  profitEstimated,
+		ProfitReal:       profitReal,
+		AfterAmountGross: afterAmountGross,
+		AfterAmountNet:   afterAmountNet,
+	}
+}
