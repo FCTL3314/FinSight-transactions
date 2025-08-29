9166e00d97475550a7e6de5c3496a02e7dc7af44
Nikita <156105775+nikita-solovyov-dev@users.noreply.github.com>
2025-08-29 17:20:41 +0300

    refactor(db): remove unnecessary comments and simplify migration scripts- Remove excessive comments from alembic.ini and env.py- Keep only essential configuration in alembic.ini- Simplify imports in env.py by using noqa comments- Remove function docstrings in env.py as they are not needed

--- migrations/env.py
+++ migrations/env.py
@@ -4,11 +4,9 @@ from alembic import context
 from src.core import settings
 from src.db.database import engine
 from src.db.models import Base
-from src.db.models.detailing import FinanceDetailing
-from src.db.models.transaction import Transaction
+from src.db.models.detailing import FinanceDetailing  # noqa
+from src.db.models.transaction import Transaction  # noqa
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
 config = context.config
 section = config.config_ini_section
 
@@ -18,25 +16,13 @@ config.set_section_option(section, "DB_PASSWORD", settings.db.password)
 config.set_section_option(section, "DB_HOST", settings.db.host)
 config.set_section_option(section, "DB_PORT", str(settings.db.port))
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
-    """Запуск миграций в режиме 'offline'."""
     url = settings.db.url
     context.configure(
         url=url,
@@ -50,7 +36,6 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Запуск миграций в режиме 'online'."""
     with engine.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
 
